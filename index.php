896d6b1652979ef2d573da335dd663389bc962f3
Alexander Peletz <APeletz512@gmail.com>
2013-09-17 21:40:37 -0400

    fine tuning

--- index.php
+++ index.php
@@ -39,8 +39,8 @@ function changeContent(display)
 <body>
 <div id="headercontainer">
 	<div id="header">
-		<h3 style="margin-top: 1em;">P1</h3>
-		<p style="text-align: right; font-size: x-small;">Created for CSCI E-15, Fall 2013<br>Harvard University Extension School</p>
+		<h3 id="title">P1</h3>
+		<p id="subtitle">Created for CSCI E-15, Fall 2013<br>Harvard University Extension School</p>
 
 	</div>
 	<div id="header2">
@@ -52,7 +52,7 @@ function changeContent(display)
 </div>
 
 <div id="maincontainer">
-	<div id="toc" >
+	<div id="toc">
 		<table>
 			<tr>
 					<td id="welcomemenu" class="menuitem" onMouseOver='changeColor("welcomemenu","header2","#2C5D3F");'>
@@ -70,14 +70,14 @@ function changeContent(display)
 					</td>
 			</tr>
 		</table>
-	</div>
+	</div>	
 		<div id="welcomecontent" class="contentitem" style="display: true;">
-			<p>Welcome to the first public website designed and maintained by Alexander Peletz. This site was created as part of my enrollment in a dynamic web applications course at the Harvard school of extension studies. </p>
+			<p>Welcome to the first public website designed and maintained by Alexander Peletz. This site was created as part of my enrollment in a dynamic web applications course at the Harvard School of Extension Studies. </p>
 			<p>Use the links to the menu to the left to navigate this site. </p>
 		</div>
 		<div id="aboutcontent" class="contentitem" style="display: none;">
 			<p>My name is Alexander Peletz. I am 24 years old and live in Andover, MA. I just started a new job as a software developer for Putnam Investments' Analytical Services department. My team is responsible for maintaining databases and generating reports and tools to assist other employees at Putnam manage their day-to-day tasks.</p>
-			<p>Before moving to Andover just last month, I spent two years working as a Technical Coordinator for Epic Systems in Madison, WI. As a Technical Coordinator I managed large-scale healthcare software implementation projects, developed tools for internal and external audiences, and provided technical support for a range of applications.</p>
+			<p>Before moving to Andover just last month, I spent two years working as a Technical Coordinator for Epic in Madison, WI. As a Technical Coordinator I managed large-scale healthcare software implementation projects, developed tools for internal and external audiences, and provided technical support for a range of applications.</p>
 			<p>I currently hold a BA degree from Emory University where I studied Economics, Middle Eastern Studies, and the Arabic language. I still love all things liberal arts and am interested in revisiting my international interests in the not-too-distant future.</p>
 			<p>My mother is a public parks administrator, tutor, and graphic designer, and my father is a professor of Anthropology. My older brother is a lighting console programmer for live productions on Broadway and my younger brother is in his senior year of highschool, hoping to attend Georgia Tech next fall.</p>
 			<p>My girlfriend of three years is in her first year at Tufts School of Medicine and is pursuing a dual MD/MPH degree.</p>
@@ -89,7 +89,7 @@ function changeContent(display)
 
 <div id="footercontainer">
 	<div id="footer">
-		<p>www.AlexanderPeletz.biz | 2013</p>
+		<p><a href="http://www.P1.AlexanderPeletz.biz">www.AlexanderPeletz.biz</a> | 2013</p>
 	</div>
 	<div id="footer2">
 	</div>
